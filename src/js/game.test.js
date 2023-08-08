637be2a2f86720631a1948279835465b563ce199
nanarama <annasampers@gmail.com>
2023-08-08 13:14:43 -0500

    Refactor and update game tests

--- src/js/game.test.js
+++ src/js/game.test.js
@@ -7,41 +7,50 @@ beforeAll(() => {
   game.gameBoards[1].placeShip([[2, 2]]);
 });
 
-test("can switch players", () => {
-  game.switchPlayers();
-  expect(game.current).toBe(1);
-  expect(game.opponent).toBe(0);
-});
-
 test("can tell when game is not over", () => {
-  let coord = game.players[game.current].makeGuess([3, 3]);
-  game.gameBoards[game.opponent].receiveAttack(coord);
   expect(game.over()).toBe(false);
-  expect(game.current).toBe(1);
-  expect(game.opponent).toBe(0);
+  expect(game.winner).toBe(false);
 });
 
 test("can tell when game is over", () => {
-  let coord = game.players[game.current].makeGuess([1, 1]);
-  let enemyShips = game.gameBoards[game.opponent].ships;
-  let enemyGameboard = game.gameBoards[game.opponent];
-  enemyGameboard.receiveAttack(coord);
-  expect(enemyShips[0].hits).toEqual([[1, 1]]);
-  expect(enemyGameboard.missedAttacks).toEqual([[3, 3]]);
+  game.playRound([2, 2]);
   expect(game.over()).toBe(true);
+  expect(game.winner).toBe(true);
 });
 
-describe("testing play funtion", () => {
+describe("testing playRound function", () => {
   beforeAll(() => {
-    game.gameBoards[0].placeShip([[4, 4]]);
-    game.switchPlayers();
+    game.gameBoards[0].placeShip([
+      [4, 4],
+      [5, 4],
+    ]);
+    game.gameBoards[1].placeShip([[5, 5]]);
   });
 
-  test("can play through rounds successfully", () => {
-    let winner = game.play();
-    expect(winner).toBe(game.players[game.current]);
-    expect(game.gameBoards[game.opponent].missedAttacks).toEqual([[3, 3]]);
-    expect(game.opponent).toBe(0);
-    expect(game.gameBoards[game.opponent].ships.length).toBe(2);
+  test("can record a miss and switchPlayers", () => {
+    expect(game.current).toBe(0);
+    let status = game.playRound([6, 6]);
+    expect(status).toBe("miss");
+    expect(game.current).toBe(1);
+  });
+
+  test("can record a hit and NOT switchPlayers", () => {
+    let status = game.playRound([4, 4]);
+    expect(status).toBe("hit");
+    expect(game.current).toBe(1);
+  });
+
+  test("can record a sunk ship and NOT switchPlayers", () => {
+    let status = game.playRound([5, 4]);
+    expect(status).toBe("sunk");
+    expect(game.current).toBe(1);
+  });
+
+  test("can set winner when game is over", () => {
+    let status = game.playRound([1, 1]);
+    let winner = game.players[game.current].name;
+    expect(status).toBe("sunk");
+    expect(game.winner).toBe(true);
+    expect(winner).toBe("Computer");
   });
 });
