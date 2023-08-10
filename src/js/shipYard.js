fac87c080bf81e92353a0461af74e101779a0668
nanarama <annasampers@gmail.com>
2023-08-10 10:46:26 -0500

    Add ShipYard and tests

---
+++ src/js/shipYard.js
@@ -0,0 +1,26 @@
+const ShipYard = {
+  shipLengths: [5, 4, 3, 3, 2],
+  orientation: "h",
+  changeOrientation() {
+    this.orientation = this.orientation == "h" ? "v" : "h";
+  },
+  nextPosition(position) {
+    if (this.orientation == "h") {
+      return [position[0], position[1] + 1];
+    } else if (this.orientation == "v") {
+      return [position[0] + 1, position[1]];
+    }
+  },
+  getTotalCoords(coord) {
+    let totalCoords = [coord];
+    for (let i = 0; i < this.shipLengths[0] - 1; i++) {
+      totalCoords.push(this.nextPosition(totalCoords[i]));
+    }
+    return totalCoords;
+  },
+  launchShip() {
+    this.shipLengths.shift();
+  },
+};
+
+export { ShipYard };
---
+++ src/js/shipYard.test.js
@@ -0,0 +1,34 @@
+import { ShipYard } from "./shipYard";
+
+test("gives correct coordinates for horizontal", () => {
+  let coords = ShipYard.getTotalCoords([1, 1]);
+  expect(coords).toEqual([
+    [1, 1],
+    [1, 2],
+    [1, 3],
+    [1, 4],
+    [1, 5],
+  ]);
+});
+
+describe("testing changeOrientation", () => {
+  beforeAll(() => {
+    ShipYard.changeOrientation();
+  });
+
+  test("gives correct coordinates for vertical", () => {
+    let coords = ShipYard.getTotalCoords([1, 1]);
+    expect(coords).toEqual([
+      [1, 1],
+      [2, 1],
+      [3, 1],
+      [4, 1],
+      [5, 1],
+    ]);
+  });
+});
+
+test("can launch ship", () => {
+  ShipYard.launchShip();
+  expect(ShipYard.shipLengths[0]).toBe(4);
+});
