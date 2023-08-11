e2d31b26a293c7092828157191734efbd9de63a8
nanarama <annasampers@gmail.com>
2023-08-11 11:22:17 -0500

    Delete placing default temporary ships for each player

--- src/js/main.js
+++ src/js/main.js
@@ -18,48 +18,7 @@ import {
 import Game from "./game";
 import { ShipYard } from "./shipYard";
 
-/*
-const player1Default = [
-  [
-    [1, 1],
-    [1, 2],
-    [1, 3],
-  ],
-  [
-    [3, 1],
-    [4, 1],
-    [5, 1],
-    [6, 1],
-  ],
-];
-
-const player2Default = [
- [
-    [2, 6],
-    [3, 6],
-    [4, 6],
-  ],
-  [
-  [8, 4],
-  [7, 4],
-],
-[
-  [5, 9],
-  [6, 9],
-  [7, 9],
-  [8, 9],
-],
-];
-*/
-
 const game = Game();
-
-/*
-player1Default.forEach((ship) => game.gameBoards[game.current].placeShip(ship));
-player2Default.forEach((ship) =>
-  game.gameBoards[game.opponent].placeShip(ship)
-);
-*/
 
 const player1 = renderBoard(
   "Player",
